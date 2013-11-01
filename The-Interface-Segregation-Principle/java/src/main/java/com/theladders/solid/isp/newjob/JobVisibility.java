6ad58e5fabc10c474e799b2430ab3a455eb0b286
Pedro Ha <pha@theladders.com>
2013-11-01 18:02:48 -0400

    Reformatting whitespace

--- The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/newjob/JobVisibility.java
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/newjob/JobVisibility.java
@@ -9,12 +9,12 @@ public interface JobVisibility
 
   boolean isConfidential();
 
-  boolean isExclusive(); // This used to add a '*' to an exclusive job
+  boolean isExclusive(); // This used to add a '*' to an exclusive TheLadder's job
 
   /**
    * Is this job a Marketing job? If this flag is set, basic access is allowed to this job (where
    * otherwise it would be premium) from certain landing pages.
-   *
+   * 
    * @return true if this is marked for marketing, false otherwise.
    */
   boolean isMarketing();
