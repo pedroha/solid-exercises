a56b3cfc8ed2ad8aae8a81e3a69ac42ffab70163
Pedro Ha <pha@theladders.com>
2013-11-12 15:38:54 -0500

    Reformatting spaces

--- The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/refactored/ArticleContentRepository.java
+++ The-Dependency-Inversion-Principle/java/src/main/java/com/theladders/solid/dip/refactored/ArticleContentRepository.java
@@ -2,30 +2,30 @@ package com.theladders.solid.dip.refactored;
 
 public class ArticleContentRepository implements ContentRepository
 {
-  private String IMAGE_PREFIX = "http://somecdnprodiver.com/static/images/careerAdvice/";
+  private String                 IMAGE_PREFIX = "http://somecdnprodiver.com/static/images/careerAdvice/";
 
-  private RepositoryManager     repositoryManager;
-  private CategoryResourceMapper        resourceMapper;
-  
+  private RepositoryManager      repositoryManager;
+  private CategoryResourceMapper resourceMapper;
+
   public ArticleContentRepository(RepositoryManager repositoryManager)
   {
     this.repositoryManager = repositoryManager;
     this.resourceMapper = new CategoryResourceMapper(IMAGE_PREFIX);
   }
-  
+
   public ContentNode getContentNode(SuggestedArticle article)
   {
     String uuid = article.getArticleExternalIdentifier();
-    
+
     // Attempt to fetch the actual content;
     ContentNode node = repositoryManager.getNodeByUuid(uuid);
-    
+
     if (node != null && ContentUtils.isPublishedAndEnabled(node))
     {
       // Override miniImagePath
       overrideMiniImagePath(node);
     }
-    return node;    
+    return node;
   }
 
   private void overrideMiniImagePath(ContentNode node)
